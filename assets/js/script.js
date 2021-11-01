22496289c0e245c7acf6202aa62d6d7f2eafc70e
tiana <tianasjohnson98@gmail.com>
2021-11-01 17:12:18 -0400

    bug fixing

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var todaysDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todaysDate);
 
 $(document).ready(function () {
+
 $(".saveBtn").on("click", function () {
 
 var taskInput = $(this).siblings(".description").val();
@@ -17,6 +18,7 @@ function currentTimeTrack () {
 
 //get current time
     var time = moment().hour();
+
     $(".time-block").each(function () {
      var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
 // change color according to time
@@ -24,14 +26,15 @@ function currentTimeTrack () {
 
         
         $(this).removeClass("present");
-        $(this).addClass("past");
         $(this).removeClass("future");
+        $(this).addClass("past");
       }
-      else if (timeBlock === time){
+      else if (timeBlock === time) {
 
-        $(this).addClass("present");
+        
         $(this).removeClass("past");
         $(this).removeClass("future");
+        $(this).addClass("present");
        
       }
       else {
@@ -43,14 +46,15 @@ function currentTimeTrack () {
 
 }
 
-$("#hour9 . description").val(localStorage.getItem("hour9"));
-$("#hour10 . description").val(localStorage.getItem("hour10"));
-$("#hour11 . description").val(localStorage.getItem("hour11"));
-$("#hour12 . description").val(localStorage.getItem("hour12"));
-$("#hour1 . description").val(localStorage.getItem("hour1"));
-$("#hour2 . description").val(localStorage.getItem("hour2"));
-$("#hour3 . description").val(localStorage.getItem("hour3"));
-$("#hour4 . description").val(localStorage.getItem("hour4"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour1 .description").val(localStorage.getItem("hour1"));
+$("#hour2 .description").val(localStorage.getItem("hour2"));
+$("#hour3 .description").val(localStorage.getItem("hour3"));
+$("#hour4 .description").val(localStorage.getItem("hour4"));
+$("#hour5 .description").val(localStorage.getItem("hour5"));
 
 currentTimeTrack();
 })
