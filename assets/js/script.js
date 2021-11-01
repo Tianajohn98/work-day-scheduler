3d2f36470d711f1001932650594c848d5cdcce24
tiana <tianasjohnson98@gmail.com>
2021-11-01 14:49:09 -0400

    change background color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,44 @@
 //time and date
 var todaysDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todaysDate);
+
+
+$(".saveBtn").on("click", function () {
+
+var taskInput = $(this).siblings(".description").val();
+
+var hour = $(this).parent().attr("id");
+localStorage.setItem(time, hour);
+
+})
+ 
+function currentTimeTrack () {
+
+//get current time
+    var time = moment().hour();
+    $(".time-block").each(function (){
+     var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+// change color according to time
+      if (timeBlock < time) {
+
+        
+        $(this).removeClass("present");
+        $(this).addClass("past");
+        $(this).removeClass("future");
+      }
+      else if (timeBlock === time){
+
+        $(this).addClass("present");
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+       
+      }
+      else {
+          $(this).removeClass("present");
+          $(this).removeClass("past");
+          (this).addClass("future");
+      }
+    })
+
+
+}
